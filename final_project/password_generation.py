288a636cddbe03eedb8743c54a1ab45482923c1f
Cascalda <111849193+Cascalda@users.noreply.github.com>
2024-03-20 21:29:09 +0800

    Revert "Added commetns to explain thought process / reasoning."

--- final_project/password_generation.py
+++ final_project/password_generation.py
@@ -23,26 +23,25 @@ WORDS = sorted(get_english_words_set(["web2"], lower=True))
 
 def main() -> None:
     """Interface to control all other functions."""
-    access_key = access_key_generators()
-    print(access_key)
+    password = password_generators()
+    print(password)
 
 
-def access_key_generators() -> str:
-    """Chooses the type of acess key the user wants."""
+def password_generators() -> str:
+    """Chooses the type of password the user wants."""
     while True:
-        # Prompt "password" instead of "access key" as "password" is more colloquial
         choice = input("\nPassword or Passphrase: ").lower()
         match = {
             "password": generate_password,
             "passphrase": generate_passphrase,
         }
+
         generator = match.get(choice, None)
 
         if generator is None:
             print("Only 'password' or 'passphrase' are accepted.")
         else:
-            access_key = generator()
-            return access_key
+            return generator()
 
 
 def generate_passphrase() -> str:
@@ -50,8 +49,6 @@ def generate_passphrase() -> str:
     length = get_valid_length(MIN_PASSPHRASE_WORDS, MAX_PASSPHRASE_WORDS)
     separator = get_separator()
 
-    # Used random.sample to avoid repeating words,
-    # making it harder to be brute-forced, especially for short passphrases
     return f"{separator}".join(random.sample(WORDS, k=length))
 
 
@@ -61,14 +58,16 @@ def generate_password() -> str:
     included_flags = get_flags()
 
     character_pool = "".join(CHARACTERS[flag] for flag in included_flags)
-
-    # Used random.choices to allow repetition, which increases unpredictability
     return "".join(random.choices(character_pool, k=length))
 
 
 def get_valid_length(min_length: int, max_length: int) -> int:
     """Obtain a valid length from the user."""
-    print(f"Length must be between {min_length} and {max_length}.")
+    print(
+        f"""
+        Length must be between {min_length} and {max_length}.
+        """
+    )
 
     morpheme_type = {
         "generate_password": "character",
@@ -77,9 +76,8 @@ def get_valid_length(min_length: int, max_length: int) -> int:
     caller = stack()[1].function
 
     while True:
-        morpheme = morpheme_type.get(caller, None)
-
         try:
+            morpheme = morpheme_type.get(caller, None)
             length = int(input(f"Enter number of {morpheme}s: "))
         except ValueError:
             print("Only integers are accepted.")
@@ -111,7 +109,7 @@ def get_flags() -> dict[bool]:
     return flags_included
 
 
-def get_separator() -> str:
+def get_separator():
     """Obtains a valid separator from the user."""
     separator_length = 1
 
