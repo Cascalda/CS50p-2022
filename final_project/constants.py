7d7b49a896d287ebada86fbb76cc40d273ea0f9f
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-03-29 17:06:16 +0800

    Added constants.py, refactored password_generation.py to follow code flow. Refactored min and max related variables to range tuple. Renamed some CONSTANTS to stick to prefix notation, and shortened some of them.

---
+++ final_project/constants.py
@@ -0,0 +1,23 @@
+"""Contains the constants that are required for password generation files."""
+
+import string
+from english_words import get_english_words_set
+
+MIN_PASSWORD_CHAR: int = 8
+MAX_PASSWORD_CHAR: int = 128
+RANGE_PASSWORD_CHAR: tuple[int, int] = (MIN_PASSWORD_CHAR, MAX_PASSWORD_CHAR)
+
+MIN_PASSPHRASE_WORDS: int = 4
+MAX_PASSPHRASE_WORDS: int = 20
+RANGE_PASSPHRASE_WORDS: tuple[int, int] = (MIN_PASSPHRASE_WORDS, MAX_PASSPHRASE_WORDS)
+
+MAX_SEPARATOR_LENGTH: int = 3
+DEFAULT_SEPARATOR: str = "_"
+
+CHARACTERS: dict[str, str] = {
+    "lowercase": string.ascii_lowercase,
+    "uppercase": string.ascii_uppercase,
+    "numbers": string.digits,
+    "special characters": string.punctuation,
+}
+WORDS: list[str] = sorted(get_english_words_set(sources=["web2"], lower=True))
