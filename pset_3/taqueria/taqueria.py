4dfa9f9afa67ad86b6bc0d6140e7d652267b2760
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-06 18:00:47 +0800

    Refactor: Moved taqueria_menu into global scope as TAQUERIA_MENU. Combined order_payment function to main() as it became too simple. Added DEFAULT_VALUE CONSTANT as well. Renamed main() to customer_order() for better relevance.

--- pset_3/taqueria/taqueria.py
+++ pset_3/taqueria/taqueria.py
@@ -1,37 +1,34 @@
 """Computes immediate cost given order."""
 
-
-def order_payment(order_obtained) -> float:
-    """Gets all the total of the order."""
-    taqueria_menu = {
-        "Baja Taco": 4.25,
-        "Burrito": 7.50,
-        "Bowl": 8.50,
-        "Nachos": 11.00,
-        "Quesadilla": 8.50,
-        "Super Burrito": 8.50,
-        "Super Quesadilla": 9.50,
-        "Taco": 3.00,
-        "Tortilla Salad": 8.00,
-    }
-
-    return taqueria_menu.get(order_obtained, 0.0)
+TAQUERIA_MENU = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00,
+}
+DEFAULT_VALUE = 0.0
 
 
-def main() -> None:
-    """Interface to control all other functions."""
+def customer_order() -> None:
+    """Provides an updated customer order."""
     total_cost = 0
 
     while True:
         try:
             order = input("Item: ").title()
-            total_cost += order_payment(order)
+            total_cost += TAQUERIA_MENU.get(order, DEFAULT_VALUE)
 
             print(f"Total: ${total_cost:.2f}")
+
         except EOFError:
             print(total_cost)
             break
 
 
 if __name__ == "__main__":
-    main()
+    customer_order()
