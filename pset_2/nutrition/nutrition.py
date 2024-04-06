422d9a0fd384261027dcfff9bee81fa4cb8b8d5a
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-06 16:59:39 +0800

    Refactor: Moved local variable 'fruits' into global scope as 'FRUITS'

--- pset_2/nutrition/nutrition.py
+++ pset_2/nutrition/nutrition.py
@@ -1,33 +1,34 @@
 """Provides calories of the given fruit"""
 
+FRUITS = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80,
+}
+
 
 def calories_of_fruit(fruit: str) -> int | str:
     """Provides calories of given fruit"""
 
-    fruits = {
-        "apple": 130,
-        "avocado": 50,
-        "banana": 110,
-        "cantaloupe": 50,
-        "grapefruit": 60,
-        "grapes": 90,
-        "honeydew melon": 50,
-        "kiwifruit": 90,
-        "lemon": 15,
-        "lime": 20,
-        "nectarine": 60,
-        "orange": 80,
-        "peach": 60,
-        "pear": 100,
-        "pineapple": 50,
-        "plums": 70,
-        "strawberries": 50,
-        "sweet cherries": 100,
-        "tangerine": 50,
-        "watermelon": 80,
-    }
     null_output = ""
-    return fruits.get(fruit, null_output)
+    return FRUITS.get(fruit, null_output)
 
 
 def main():
