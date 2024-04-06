f5e8fe62b4e6b82866d71af87cc4daa893830d74
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-06 16:57:58 +0800

    Refactor: Gave a CONSTANT named SEPARATOR to '_' as a snake case constant

--- pset_2/camel/camel.py
+++ pset_2/camel/camel.py
@@ -1,5 +1,7 @@
 """Convert camelCase characters into snake case."""
 
+SEPARATOR = "_"
+
 
 def snake_caser(camel_case: str) -> str:
     """Brings the snake_case out of camelCase."""
@@ -10,7 +12,7 @@ def snake_caser(camel_case: str) -> str:
         if char.islower():
             snake_cased += char
         else:
-            to_snake_case = f"_{char.lower()}"
+            to_snake_case = f"{SEPARATOR}{char.lower()}"
             snake_cased += to_snake_case
 
     return snake_cased
