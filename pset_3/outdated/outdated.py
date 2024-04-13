63ba790afc8ffc030f412fcb40f958cc29cb83e5
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-13 12:04:56 +0800

    Refactor: Wanted to implement DRY on this but found to be too simple to modularise. Locality of behaviour is better here. Only refactored out the actual converting to iso format part, renamed the original middle_to_big_endian to just extract_date, and introduced a custom Exception InvalidDateError to catch generally what would be invalid dates.

--- pset_3/outdated/outdated.py
+++ pset_3/outdated/outdated.py
@@ -1,4 +1,4 @@
-"""Changes middle endian dates to big endian dates."""
+"""Changes middle endian dates to big endian dates"""
 
 ACCEPTED_MONTHS = {
     "January": "1",
@@ -16,41 +16,56 @@ ACCEPTED_MONTHS = {
 }
 
 
-def date_middle_endian_to_big_endian(date: str) -> str:
-    """Formats American dates to ISO dates."""
+class InvalidDateError(Exception):
+    """Used for catching invalid delimiters, month names, and dates."""
+
+
+def extract_date(date: str) -> tuple[str, str, str]:
+    """Formats American dates to ISO dates"""
+    year, month, day = "", "", ""
+
     # Extracting day, month, year while checking for format validity
     if "/" in date:
         month, day, year = date.split("/")
+        if not (month.isdigit() and day.isdigit and year.isdigit()):
+            raise InvalidDateError("Only numbers allowed in MM/DD/YYYY format.")
 
     elif "," in date:
         month_day, year = date.split(",")
         month, day = month_day.split()
         month = ACCEPTED_MONTHS.get(month)
         if not month:
-            raise RuntimeError("Invalid Month")
+            raise InvalidDateError("No such month name.")
+
     else:
-        raise RuntimeError("Invalid Delimiters")
+        raise InvalidDateError("Delimiter not accepted.")
 
     # Checking for date validity
+    # Assumes date parts are always in numeric form
     if int(day) >= 31 or int(month) > 12:
-        raise RuntimeError("Invalid Date")
+        raise InvalidDateError("Invalid day or month.")
 
-    # Date formatting
+    return (year, month, day)
+
+
+def convert_to_iso(year, month, day):
+    """Convert the date to iso format."""
     return f"{year}-{int(month):02d}-{int(day):02d}"
 
 
-def main() -> None:
-    """Interface to control all other functions."""
+def main():
+    """Interface to control all other functions"""
 
     while True:
-        date_user = input("Date: ").strip()
+        date = input("Date (MM/DD/YYYY or Month DD, YYYY): ").strip()
 
         try:
-            result = date_middle_endian_to_big_endian(date_user)
-            print(result)
+            year, month, day = extract_date(date)
+            iso_date = convert_to_iso(year, month, day)
+            print(iso_date)
             break
 
-        except (ValueError, RuntimeError) as e:
+        except InvalidDateError as e:
             print(e)
 
 
