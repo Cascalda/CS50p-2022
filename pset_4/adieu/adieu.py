efb98576760aaee797d2d16e94279e45a4034a22
Granite7 <111849193+Cascalda@users.noreply.github.com>
2024-04-14 17:38:49 +0800

    Refactor: Removed unnecessary type casting of name variable to tuple

--- pset_4/adieu/adieu.py
+++ pset_4/adieu/adieu.py
@@ -1,20 +1,19 @@
-"""Make the So Long, Farewell song in The Sound of Music have proper oxford commas."""
+"""Make the So Long, Farewell song in The Sound of Music have proper oxford commas"""
 
 import inflect
 
 inflect = inflect.engine()
 
 
-def oxford_comma() -> None:
-    """Forms a sentence with oxford commas."""
-    names = []
-
+def oxford_comma():
+    """Forms a sentence with oxford commas"""
     try:
+        names = []
+
         while True:
             name = input("Name: ")
             names.append(name)
     except EOFError:
-        names = tuple(names)
         to_oxford_comma = inflect.join(names)
         print(f"\nAdieu, adieu, to {to_oxford_comma}")
 
